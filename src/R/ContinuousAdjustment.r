98855003e37eb2434e0952d04b3651a5f913b88f
jtho139 <jtho139@ARTSW256605.UoA.auckland.ac.nz>
2012-07-24 12:04:32 +1200

    Delete incorrect sentences in blurb about continuous adjustments method.

--- src/R/ContinuousAdjustment.r
+++ src/R/ContinuousAdjustment.r
@@ -4,15 +4,7 @@
 #' Eg: a user may wish to decrease the number of cigarettes smoked per day by 20 for every 
 #' child with a mother who smokes 40 or more cigarettes a day) 
 #' The user specifies from the user interface desired increments (or decrements) for all micro units
-#' in particular categories.  These adjustments are stored in the cont.adjustments matrix.
-#' The filled in cont.adjustments matrix (filling in done elsewhere) is then used as input to a function
-#' that 1) applies these user-requested adjustments to the stored base runs (cont.outcomes.all.runs) to 
-#' create fixed.cont.outcomes
-#' and 2) creates the typical distributions that would be expected across the categorised continuous
-#' variables after making the adjustments (the typical distributions are calculated from the 
-#' fixed.cont.outcomes). 
-#' At each iteration in the simulation, for variables for which adjustments were made, the 
-#' fixed.cont.outcomes are used instead of the simulated values at that iteration. 
+#' in particular categories.  
 #' 
 #' Create empty continuous variable adjustment matrices.
 #' Initial matrix values are NA (i.e: no adjustment).
